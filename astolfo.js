cf06e5a0297bcfc2e8f2c8b78f62c9b0ec2676e6
Natsu-sky <92406380+natsu-sky@users.noreply.github.com>
2022-07-27 23:51:24 -0400

    1st thing lmao
    According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway. Because bees don’t care what humans think is impossible

---
+++ astolfo.js
@@ -0,0 +1,6 @@
+const server = require('http').createServer();
+server.on("request", (request, response) => {
+  response.writeHead(200);
+  response.end("Astolfo is hot.", "utf-8");
+});
+server.listen(80);
