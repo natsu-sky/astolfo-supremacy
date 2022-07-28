37ba02418837e795d7cdc6bc786168bc9078e819
Natsu-sky <92406380+natsu-sky@users.noreply.github.com>
2022-07-28 00:56:47 -0400

    Update astolfo.js

--- astolfo.js
+++ astolfo.js
@@ -1,6 +1,6 @@
 const server = require('http').createServer();
 server.on("request", (request, response) => {
   response.writeHead(200);
-  response.end(<img src="https://cdn.discordapp.com/attachments/939560697090678824/1002068164131176570/wallpaperflare.com_wallpaper.jpg"></img>, "utf-8");
+  response.end("Astolfo is hot.", "utf-8");
 });
 server.listen(process.env.PORT);
