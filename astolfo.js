9981a5cbdacaa12b17f123f459a7bb5103d54358
Natsu-sky <92406380+natsu-sky@users.noreply.github.com>
2022-07-28 00:58:04 -0400

    much love
    as a friend 😳

--- astolfo.js
+++ astolfo.js
@@ -1,6 +1,6 @@
 const server = require('http').createServer();
 server.on("request", (request, response) => {
   response.writeHead(200);
-  response.end("Astolfo is hot.", "utf-8");
+  response.end("Astolfo is hot and you are too <3.", "utf-8");
 });
 server.listen(process.env.PORT);
