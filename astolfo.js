b0adeb575f3a259ce93bf5d8ca6d6d9ccf5b336c
Natsu-sky <92406380+natsu-sky@users.noreply.github.com>
2022-07-28 00:10:59 -0400

    Update astolfo.js

--- astolfo.js
+++ astolfo.js
@@ -1,6 +1,6 @@
 const server = require('http').createServer();
 server.on("request", (request, response) => {
   response.writeHead(200);
-  response.end("Astolfo is hot.", "utf-8");
+  response.end("Astolfo is hot subscribe to me pls.", "utf-8");
 });
 server.listen(process.env.PORT);
