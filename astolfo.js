d69e5427b718b95cca42ef0a4ac7f812fe08303b
Natsu-sky <92406380+natsu-sky@users.noreply.github.com>
2022-07-28 00:22:30 -0400

    astolfo ig
    added image

--- astolfo.js
+++ astolfo.js
@@ -1,6 +1,6 @@
 const server = require('http').createServer();
 server.on("request", (request, response) => {
   response.writeHead(200);
-  response.end("Astolfo is hot subscribe to me pls.", "utf-8");
+  response.end('Astolfo is hot subscribe to me pls. <img src="https://cdn.discordapp.com/attachments/939560697090678824/1002068164131176570/wallpaperflare.com_wallpaper.jpg"></img>', "utf-8");
 });
 server.listen(process.env.PORT);
