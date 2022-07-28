b82e02e5b208c5c370a82fc9158dffaa8f43f788
Natsu-sky <92406380+natsu-sky@users.noreply.github.com>
2022-07-28 00:38:07 -0400

    Update astolfo.js
    maybe a thing

--- astolfo.js
+++ astolfo.js
@@ -1,6 +1,7 @@
 const server = require('http').createServer();
 server.on("request", (request, response) => {
   response.writeHead(200);
-  response.end('Astolfo is hot subscribe to me pls. <img src=https://cdn.discordapp.com/attachments/939560697090678824/1002068164131176570/wallpaperflare.com_wallpaper.jpg></img>', "utf-8");
+  response.end('Astolfo is hot subscribe to me pls.
+<img src="https://cdn.discordapp.com/attachments/939560697090678824/1002068164131176570/wallpaperflare.com_wallpaper.jpg" alt="Girl in a jacket" width="500" height="600">', "utf-8");
 });
 server.listen(process.env.PORT);
